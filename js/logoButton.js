a82ab433f98f13454451fdc95b0a96ec5c31044a
maxitect <maxime.downe@gmail.com>
2024-09-28 00:38:12 +0100

    fix: accessibility improvements

--- js/logoButton.js
+++ js/logoButton.js
@@ -1,4 +1,15 @@
-// Add event listener to SVG to reset color scheme when clicked
-document.getElementById('logo-reset-button').addEventListener('click', () => {
-    setColourScheme();  // Resets the colors when SVG is clicked
+// Add event listener for clicks and keyboard interaction
+const logoButton = document.getElementById('logo-reset-button');
+if (logoButton) {
+  // Reset color scheme on click
+  logoButton.addEventListener('click', () => {
+    setColourScheme();
+  })
+};
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'c') {
+        event.preventDefault();  // Optional: prevent any default Enter behavior
+        setColourScheme();       // Call the function to reset the color scheme
+    }
 });
