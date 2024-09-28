cbf6cd351cbd731bfc8dd12ba0867c25fe8a4cba
maxitect <maxime.downe@gmail.com>
2024-09-28 18:15:54 +0100

    fix: project images matching colour scheme variation using transparent png

--- js/generateBoxes.js
+++ js/generateBoxes.js
@@ -1,4 +1,31 @@
-fetchData('project-boxes')
+const container = document.querySelector('.flex-container'); // Select the container div
+
+fetchData('info-boxes')
+
+// Loop through the events data
+.then((infos) => {
+    infos.forEach(info => {
+    // Create the flex item container
+    const flexItem = document.createElement('div');
+    flexItem.className = "flex-item";
+
+    // Create and set the title div element
+    const title = document.createElement('h2');
+    title.textContent = info.title;    
+
+    // Append all created elements to the flex item container
+    flexItem.appendChild(title);
+    for (para of info.content) {
+        const paragraph = document.createElement('p');
+        paragraph.innerHTML = para;
+        flexItem.appendChild(paragraph);
+    }
+
+    // Append the flex item into the flex container
+    container.appendChild(flexItem);
+    }
+)})
+.then(() => (fetchData('project-boxes')
 
 // Loop through the events data
 .then((projects) => {
@@ -29,4 +56,4 @@ fetchData('project-boxes')
     // Append the flex item into the flex container
     container.appendChild(flexItem);
     }
-)});
+)})));
